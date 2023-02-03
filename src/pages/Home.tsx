53835b92e78ad2403e91697eb1ecfc01769fdc1e
mirzaahmedov.dev <mirzaahmedov.dev@gmail.com>
2023-02-03 16:29:59 +0500

    some examples

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import TextField from "@/components/Textfield";
 import Text from "@/components/Text";
 
 import { LocationIcon } from "@/assets/icons";
+import { getTodaysWeather } from "@/api/queries/forecast";
 
 const Container = styled.div`
   padding: 2.5rem;
@@ -12,8 +15,44 @@ const Header = styled.header`
   align-items: center;
   justify-content: center;
 `;
+const WeatherToday = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  max-width: 150px;
+`;
 
 const Home = () => {
+  const [position, setPosition] = useState<{ x: number; y: number } | null>(
+    null
+  );
+
+  const { data, isLoading, isError } = useQuery(
+    ["weather", position?.x, position?.y],
+    () => getTodaysWeather(position!),
+    { enabled: !!position }
+  );
+
+  console.log(data);
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setPosition({
+          x: position.coords.latitude,
+          y: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+
   return (
     <Container>
       <Text variant="bs-bold">Text</Text>
@@ -24,8 +63,11 @@ const Home = () => {
           icon={<LocationIcon />}
         />
       </Header>
+      <WeatherToday>
+        <Image src="/images/partly_day_storm_light.png" alt="Cloud Light" />
+      </WeatherToday>
       <Text as="h1" variant="xl-bold">
-        Heading
+        {Array.isArray(data?.hourly) ? data?.hourly.temperature_2m[0] : null}°
       </Text>
     </Container>
   );
