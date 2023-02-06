0ff952179bab80bfb21f46624e96da2ebb217271
“Islombek20” <dilbarm7@gmail.com>
2023-02-06 12:09:45 +0500

    card

---
+++ src/components/Card.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled, { css, useTheme } from 'styled-components';
+import Flex from './Flex';
+import Text from './Text';
+
+interface WeatherData {
+  date: string;
+  temperature: {
+    daytime: string;
+    evening: string;
+  };
+  weatherType: string;
+}
+
+type Props = {
+  variant: CardVariant;
+  data: WeatherData;
+};
+
+const CardContainer = styled.div<Props>`
+  background: #60a5fa;
+  border-radius: 20px;
+
+  ${({ variant }) => {
+    if (variant === 'md') {
+      return css`
+        padding: 20px;
+      `;
+    } else {
+      return css`
+        max-width: 148px;
+        max-height: 180px;
+        padding: 10px 20px;
+      `;
+    }
+  }};
+`;
+
+const Temperature = styled.div<Props>`
+  ${({ variant }) => {
+    if (variant === 'md') {
+      return css``;
+    }
+  }}
+
+  & > ${Text}:last-child {
+    color: red;
+  }
+`;
+
+type CardVariant = 'sm' | 'md';
+
+const Card = (props: Props) => {
+  const { data } = props;
+  const { colors } = useTheme();
+
+  return (
+    <CardContainer {...props}>
+      <Flex direction="column">
+        <Text variant="bs-normal" color={colors.blue['200']}>
+          {data.date}
+        </Text>
+        <Temperature {...props}>
+          <Text variant="bs-normal" color={colors.blue['200']}>
+            {data.temperature.daytime}
+          </Text>
+          <Text>{data.temperature.evening}</Text>
+        </Temperature>
+      </Flex>
+    </CardContainer>
+  );
+};
+
+export default Card;
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,12 +1,12 @@
-import { BaseHTMLAttributes, ElementType, ReactNode } from "react";
-import styled, { css } from "styled-components";
+import { BaseHTMLAttributes, ElementType, ReactNode } from 'react';
+import styled, { css } from 'styled-components';
 
 const sizes = {
-  sm: "0.75rem",
-  bs: "0.875rem",
-  md: "1.5rem",
-  lg: "2rem",
-  xl: "4rem",
+  sm: '0.75rem',
+  bs: '0.875rem',
+  md: '1.5rem',
+  lg: '2rem',
+  xl: '4rem',
 };
 const weights = {
   normal: 400,
@@ -20,26 +20,21 @@ type Variants = `${Size}-${Weight}`;
 
 type Props = {
   variant?: Variants;
+  color?: string;
   children?: ReactNode;
   as?: ElementType;
 };
 
-const Element = styled.p<Props>`
-  ${({ variant }) => {
-    const [size, weight] = (variant?.split("-") as [Size, Weight]) ?? [];
+const Text = styled.p<Props>`
+  ${({ variant, color }) => {
+    const [size, weight] = (variant?.split('-') as [Size, Weight]) ?? [];
 
     return css`
       font-size: ${sizes[size]};
+      color: ${color};
       font-weight: ${weights[weight]};
     `;
   }};
 `;
-
-const Text = ({
-  children,
-  ...props
-}: BaseHTMLAttributes<HTMLParagraphElement> & Props) => {
-  return <Element {...props}>{children}</Element>;
-};
 
 export default Text;
